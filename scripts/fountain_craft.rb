dca06da027b8bd015f7d771d26e766d949f9b1a1
Paul Morganthall <paul@morganthall.com>
2012-12-18 22:34:44 -0500

    Symbolize the commands to craftshop.

--- scripts/fountain_craft.rb
+++ scripts/fountain_craft.rb
@@ -2,8 +2,8 @@ farms = :home, :english, :lighthouse, :winter, :hawaiian, :jade, :haunted
 
 farms.each do |farm|
   travel_to farm
-  craftshop open
-  3.times { craftshop get_it }
-  craftshop craft, :drill_bit, :copper_tube, :cut_bamboo
-  craftshop close
+  craftshop :look_inside
+  3.times { craftshop :get_it }
+  craftshop :craft, :drill_bit, :copper_tube, :cut_bamboo
+  craftshop :close
 end
