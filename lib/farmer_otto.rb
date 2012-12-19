87cdc7365b326e6998d8677a31f5a4375eaf502b
Paul Morganthall <paul@morganthall.com>
2012-12-18 23:10:04 -0500

    implement details of #travel_to
    with help from farm.yaml

--- lib/farmer_otto.rb
+++ lib/farmer_otto.rb
@@ -1,7 +1,9 @@
+require 'yaml'
+
 class FarmerOtto
 
   def initialize
-
+    @settings = YAML::load_file('farm.yaml')
   end
 
   def run_script(path)
@@ -10,10 +12,29 @@ class FarmerOtto
 
   def travel_to(farm)
     puts "travelling to #{farm}"
+    @current_farm ||= farm
+    return farm if farm == @current_farm
+
+    click :travel_button
+    get_route_to(farm).each do |step|
+      click step, 0.5
+    end
+    sleep 1.0  # sleep 14.0
+    click :close_farm_cash_offer
   end
 
   def craftshop(*args)
     puts "craft shop performing: #{args.inspect}"
+  end
+
+  private
+
+  def get_route_to(farm)
+    @settings.fetch('routes').fetch(farm.to_s)
+  end
+
+  def click(spot, wait=0)
+    puts "click: #{spot} and wait: #{wait}"
   end
 
 end
