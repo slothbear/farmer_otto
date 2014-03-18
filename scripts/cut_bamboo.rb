03eec672c9d0b3037cf749c7e77f60c2cbd4ef43
Paul Morganthall <paul@morganthall.com>
2014-03-18 12:00:16 -0400

    Use newer features.
    1. activate_farm
    2. close_farm_cash_offer
    3. craftshop :count option

--- scripts/cut_bamboo.rb
+++ scripts/cut_bamboo.rb
@@ -1,11 +1,13 @@
-puts "sleeping 5"
-sleep 5.0
-craftshop :open
+wait 0.1
+activate_farm
 
-8.times do
-  craftshop :get_it
+# There are currently 15 farms with standard craftshops.
+15.times do
+  travel_to_least_recent_farm
+  close_farm_cash_offer
+
+  craftshop :open
+  craftshop :get_it,    count: 8
+  craftshop :cut_bamboo, count: 8
+  craftshop :close
 end
-
-craftshop :cut_bamboo, count: 8
-
-craftshop :close
