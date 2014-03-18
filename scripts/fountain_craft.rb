96265a5f9618d37cfe9606c0a4f8e7b3d179e81e
Paul Morganthall <paul@morganthall.com>
2014-03-18 11:59:07 -0400

    Update fountain craft example to use new features.
    1. activate_farm
    2. close_farm_cash_offer
    3. craftshop count: option (instead of manual loop)
    
    Note this script isn't currently in use – there are separate 
    scripts per craft item. Especially since page/row/column
    parameters support generic crafting of items.
    
    This script may be obsolete.

--- scripts/fountain_craft.rb
+++ scripts/fountain_craft.rb
@@ -1,23 +1,17 @@
 sleep 5.0
+activate_farm
 
 standard_farm_count.times do
   travel_to_least_recent_farm
+  close_farm_cash_offer
   craftshop :open
-
-  # If your craftshop has fewer than 8 stations, the extra
-  # get_it clicks will land in neutral space.
-  8.times do
-    craftshop :get_it
-  end
+  craftshop :get_it, count: 8
 
   # A Wishing Fountain stage requires twice as many drill bits
   # as the other parts; make parts in that proportion.
-  # craftshop :drill_bit,   count: 4
-  # craftshop :cut_bamboo,  count: 2
-  # craftshop :copper_tube, count: 2
-
-  # Making one kind of part at a time needs fewer kinds of bushels.
-  craftshop :drill_bit, count: 8
+  craftshop :drill_bit,   count: 4
+  craftshop :cut_bamboo,  count: 2
+  craftshop :copper_tube, count: 2
 
   craftshop :close
 end
