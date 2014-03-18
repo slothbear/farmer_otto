a364561b6592aa7e954b098dc687dbe6e5fded4b
Paul Morganthall <paul@morganthall.com>
2014-03-18 12:26:55 -0400

    Experimentation with displaying farm codes.

--- scripts/all_famrs.rb
+++ scripts/all_famrs.rb
@@ -1,1 +1,10 @@
 all_farms
+
+# support for this script - torn from lib/farmer_otto.rb
+
+def all_farms
+  puts "farm codes\n\n"
+  puts @settings['farm_codes']
+  puts "other farms\n\n"
+  puts @settings['side_farms']
+end
