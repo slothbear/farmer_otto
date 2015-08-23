8caa9b8525a0e8292efbed14896cac89215dac6d
Paul Morganthall <paul@morganthall.com>
2015-08-23 13:39:04 -0400

    Update Herbal Inn spots

--- scripts/ss_golden_honey_potion.rb
+++ scripts/ss_golden_honey_potion.rb
@@ -1,7 +1,7 @@
 wait 0.1
 
 # Harvest the ready golden honey potions.
-spot = [80, 108]
+spot = [40, 108]
 4.times do |count|
   y = spot[1]+count*78
   puts "next harvest: #{[spot[0], y]}"
@@ -10,12 +10,18 @@ end
 
 # Clear the requests to share.
 4.times do
-  click [410, 390], 1.0
+  click [410, 385], 1.0
 end
 
 # Make more.
 4.times do
-  # make = [630, 256] # spot 1 or .first
-  make = [630, 360] # spot 2 or make.second
+  # make = [608, 245] # spot 1 or .first
+  make = [608, 356] # spot 2 or make.second
+  # make = [608, 474]
   click make, 1.0
+end
+
+# Clear any level up messages.
+2.times do
+  click [410, 385], 1.0
 end
