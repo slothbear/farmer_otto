99dd1f521ffa5ae980a7af0b809380c9742250fe
Paul Morganthall <paul@morganthall.com>
2015-08-25 11:56:52 -0400

    remove magic numbers from script

--- scripts/ss_golden_honey_potion.rb
+++ scripts/ss_golden_honey_potion.rb
@@ -1,27 +1,37 @@
+GOODS_COUNT = 5
+
+def finish_goods
+  # Harvest the ready golden honey potions.
+  spot = [40, 108]
+  GOODS_COUNT.times do |count|
+    y = spot[1]+count*78
+    puts "next harvest: #{[spot[0], y]}"
+    click [spot[0], y], 5.0
+  end
+
+  # Clear the requests to share.
+  GOODS_COUNT.times do
+    click [410, 385], 2.0
+  end
+
+  # Make more.
+  GOODS_COUNT.times do
+    make = [608, 245] # spot 1 or .first
+    # make = [608, 356] # spot 2 or make.second
+    # make = [608, 474]
+    click make, 2.0
+  end
+
+  # Clear any level up messages.
+  2.times do
+    click [410, 385], 1.0
+  end
+end
+
+#main
 wait 0.1
+12.times do
+  finish_goods
+  wait 61.0
 
-# Harvest the ready golden honey potions.
-spot = [40, 108]
-4.times do |count|
-  y = spot[1]+count*78
-  puts "next harvest: #{[spot[0], y]}"
-  click [spot[0], y], 3.0
-end
-
-# Clear the requests to share.
-4.times do
-  click [410, 385], 1.0
-end
-
-# Make more.
-4.times do
-  # make = [608, 245] # spot 1 or .first
-  make = [608, 356] # spot 2 or make.second
-  # make = [608, 474]
-  click make, 1.0
-end
-
-# Clear any level up messages.
-2.times do
-  click [410, 385], 1.0
 end
---
+++ scripts/use_turbos.rb
@@ -0,0 +1,7 @@
+sleep 5
+
+22.times do
+  click [680, 566], 0.5  # gift menu
+  click [680, 513], 1.5  # gift box
+  click [232, 322], 1.0  # use spot #1, turbo
+end
